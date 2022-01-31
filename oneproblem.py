d07073303165dea443702ea5432bda8d11720b32
Mohammed9022 <afsaridrisi13@gmail.com>
2022-01-31 22:21:18 +0530

    creting a list of fruits usinf list method

--- oneproblem.py
+++ oneproblem.py
@@ -84,3 +84,16 @@ dated = str(date.today())
 letter = letter.replace("<|NAME|>", name)
 letter = letter.replace("<|DATE|>", dated)
 print(letter)
+
+# Write a program to enter seven fruits names in a list entered by the user.
+
+f1 = input("Enter Fruit Number 1: ")
+f2 = input("Enter Fruit Number 2: ")
+f3 = input("Enter Fruit Number 3: ")
+f4 = input("Enter Fruit Number 4: ")
+f5 = input("Enter Fruit Number 5: ")
+f6 = input("Enter Fruit Number 6: ")
+f7 = input("Enter Fruit Number 7: ")
+
+myList = [f1, f2, f3, f4, f5, f6, f7]
+print(myList)
