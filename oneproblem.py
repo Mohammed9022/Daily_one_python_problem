f4a1ffcbf754220221e06cbad0cae90fbb7c1837
Mohammed9022 <afsaridrisi13@gmail.com>
2022-02-03 00:49:26 +0530

    Working on conditional arguments

--- oneproblem.py
+++ oneproblem.py
@@ -190,3 +190,100 @@ print(favDict)
 #s = {8, 7, 12, "Afsar", [1, 2]}
 #print(s) # This will throw an TypeError(unhashable type: 'list') 
 # For this example we cannot change the list items in this set because sets are immutable.
+
+## Write a program to find greatest of four numbers entered by the user.
+
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+num3 = int(input("Enter number 3: "))
+num4 = int(input("Enter number 4: "))
+
+if (num1 > num4):
+	f1 = num1
+else:
+	f1 = num4
+
+if (num2 > num3):
+	f2 = num2
+else:
+	f2 = num3
+
+
+if (f1 > f2):
+	print(str(f1) + " is greatest")
+else:
+	print(str(f2) + " is greatest")
+
+
+
+# Write a program to find out wheather a student is pass or fail. If it requires total 40% and
+# at least 33% in each subject to pass. Assume 3 subjects and take marks as an input from the user.
+
+
+sub1 = int(input("Enter first subject marks\n")) 
+sub2 = int(input("Enter second subject marks\n")) 
+sub3 = int(input("Enter third subject marks\n")) 
+
+if (sub1 < 33 or sub2 < 33 or sub3 < 33):
+	print("You are fail because you have less than 33% in one of the subjects")
+
+elif (sub1 + sub2 + sub3 ) / 3 < 40:
+	print("You are fail due to total percentage less than 40")
+else:
+	print("Congratualations! You have passed the exam")
+
+
+
+# Write a program to detect these spam words. A spam comment is definded as a text containing following keywords:
+#("make a lot of money", "buy now", "subscribe now", "click this")
+
+text = input("Enter the text\n")
+
+spam = False # If we have commented this line so we have to use else part of the ladder.
+
+if ("make a lot of money" in text):
+	spam = True
+
+elif ("buy now" in text):
+	spam = True
+
+elif ("click this" in text):
+	spam = True
+
+elif ("subscribe this" in text):
+	spam = True
+
+# else:  # If we use spam=False in the top then we dont use these 2 lines.
+# 	spam = False
+
+if(spam):
+	print("This text is spam")
+else:
+	print("This text is not spam")
+
+
+# Write a program to find wheater a given username contains less thann 10 characters or not.
+
+username = input("Enter username here: ")
+
+length = len(username)
+
+if (length < 10 ):
+	print("Given characters are less than 10")
+
+else:
+	print("Given characters are greater than 10")
+
+
+
+# Write a program to find given words are present in the list or not.
+
+list_of_names = ["priyanka", "rizaa", "rakesh", "afsar", "faisal"]
+
+name = input("Enter the name to check \n")
+
+if (name in list_of_names):
+	print("Your name is present in the list")
+else:
+	print("Your name is not present in the list")
+
