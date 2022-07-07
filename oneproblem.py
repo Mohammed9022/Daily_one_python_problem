e7516c23b9e0d114487c5ca3151716166f3d2f06
Mohammed9022 <afsaridrisi13@gmail.com>
2022-07-07 20:43:58 +0530

    changes made

--- oneproblem.py
+++ oneproblem.py
@@ -17,7 +17,7 @@ Twinkle, twinkle, little star,
 	How I wonder what you are""") 
 
 #another way is this.
-print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are")
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \n\tTwinkle, twinkle, little star, \n\tHow I wonder what you are")
 
 
 
